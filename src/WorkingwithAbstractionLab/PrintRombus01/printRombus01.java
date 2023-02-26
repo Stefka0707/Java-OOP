8ebcd90f80ebe81769642174d440e54af53c3295
Stefka0707 <stefka0707@gmail.com>
2023-02-26 15:21:20 +0200

    WorkingWithAbstractionLab

---
+++ src/WorkingwithAbstractionLab/PrintRombus01/printRombus01.java
@@ -0,0 +1,33 @@
+package WorkingwithAbstractionLab.PrintRombus01;
+
+import java.util.Scanner;
+
+public class printRombus01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+
+        for (int i = 1; i <= n; i++) {
+            printRow(i,n-i);
+
+        }
+        for (int i = n - 1; i > 0; i--) {
+            printRow(i,n-i);
+        }
+
+    }
+
+    private static void printRow(int numberOfStars,int numberOfLeadingSpaces) {
+        for (int i = 0; i <numberOfLeadingSpaces ; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < numberOfStars; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+
+    }
+}
